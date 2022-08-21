080885a26ef13557be8823ac0671e636db16871b
georgemwangi118 <gmwangi118@yahoo.com>
2022-08-21 12:23:29 +0300

    added api

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home({ pizzaList }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Featured />
-      <PizzaList pizzaList={pizzaList} />
+      {/*<PizzaList pizzaList={pizzaList} />*/}
     </div>
   );
 }
